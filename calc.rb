4cbc70c32a93d1ab1cc21b02886022e7115a97e4
lipcheyy <vladlipchey05@gmail.com>
2022-03-22 16:09:07 +0200

    git commit -m 'feat:add primes'

--- calc.rb
+++ calc.rb
@@ -5,12 +5,20 @@ def factorial(n)
         return 1;
     end
 end
+def prime_numbers(value,b)
+    prime_numbers = []
+    (value..b).each do |number|
+        prime_numbers << number if is_prime(number)
+    end
+    prime_numbers
+  end
 class Calc
     puts "Enter the first value"
     val=gets.chomp.to_i
     go_next=true
     puts "Enter +,-,/,*,**,--,++,!,sqrt,sin,ctan,tan,cos,exp,ln,mod"
     mw=0;
+    stack=[];
     while go_next
         s=gets.chomp
         if s=='+' #Додавання
@@ -203,7 +211,53 @@ class Calc
                 go_next=false
             else puts "Enter +,-,/,*,**,--,++,!,sqrt,sin,ctan,tan,cos,exp,ln,mod"
             end
-         else
+        elsif s=="push"
+            stack<<val;
+            puts stack.last(1)
+            puts "Do you want to continue? 'yes or no"
+            choice=gets.chomp;
+            if(choice=="no")
+                go_next=false
+            else
+                puts "Enter +,-,/,*,**,--,++,!,sqrt,sin,ctan,tan,cos,exp,ln,mod"
+            end
+        elsif s=="con" || choice=="con"
+            puts "Enter the value you want to push in stack"
+            val=gets.chomp.to_i
+            stack<<val
+            puts "Do you want to continue? 'yes,no or con to continue to fill an array?'"
+            choice = gets.chomp
+            if(choice=="no")
+                go_next=false
+            else
+                puts "Enter +,-,/,*,**,--,++,!,sqrt,sin,ctan,tan,cos,exp,ln,mod"
+            end
+        elsif s=="pop"
+            puts stack.last(1)
+            stack.pop
+            if stack.length==0
+                puts "Error, stack is empty"
+            end
+            puts "Do you want to continue? 'yes or no"
+            choice = gets.chomp
+            if(choice=="no")
+                go_next=false
+            else puts "Enter +,-,/,*,**,--,++,!,sqrt,sin,ctan,tan,cos,exp,ln,mod"
+            end
+        elsif s=="stack"
+            puts stack
+            puts "Do you want to continue? 'yes or no'"
+            choice = gets.chomp
+            if(choice=="no")
+                go_next=false
+            else puts "Enter +,-,/,*,**,--,++,!,sqrt,sin,ctan,tan,cos,exp,ln,mod"
+            end
+        elsif s== "primes"
+            b=gets.chomp.to_i
+            puts prime_numbers(b)
+            
+        
+        else
             puts "error"
             puts "Enter +,-,/,*,**,--,++,!,sqrt,sin,ctan,tan,cos,exp,ln,mod"
         end
